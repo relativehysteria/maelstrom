83d23d1c6811cec9e190ced19819bc0a8efd1f48
relativehysteria <rruziro@gmail.com>
2025-06-24 01:27:10 +0200

    fault tolerant broadcast

--- src/services/broadcast.rs
+++ src/services/broadcast.rs
@@ -1,18 +1,25 @@
 //! Broadcast broadcast broadcast broadcast broadcast broadcast
 //!
-//! This implementation does NOT handle lost packets well! In a real
-//! microservice cluster, all nodes would be connected in such a way that a lost
-//! message from one node would not hinder the network, because that same
-//! message would be sent by another node. There are many corner cases, the
-//! simplest one is a leaf node, i.e. a node with only one neighbor. If a
-//! broadcast packet is lost in this connection, that broadcast will be lost
-//! on the recipient forever, because we choose to ignore `BroadcastOk` packets.
-//!
+//! This implementation is fault tolerant but not perfectly optimized.
+//! Every `GOSSIP_TIME`, each node sends out a read request to its neighbors,
+//! which causes the neighbors to send out _all_ of the messages they've seen up
+//! to that point. There are better ways to implement this; the best i can think
+//! of is to simply retry unacknowledged broadcasts.
+//! This implementation however is much more simpler and aligns well with our
+//! architecture up to this point. And it works!
+
+use std::time::{Instant, Duration};
 use std::io::Write;
 use std::collections::{HashMap, HashSet};
 use serde::{Serialize, Deserialize};
 use crate::message::Message;
 use crate::node::{Node, NodeInfo};
+
+/// How often a node gossips with its neighbors.
+///
+/// Every `GOSSIP_TIME` interval, a node sends a read request to its neighbors
+/// to sync their latest seen values.
+const GOSSIP_TIME: Duration = Duration::from_millis(300);
 
 /// Payload for the broadcast part of the challenge. This includes all of the
 /// possible message payloads
@@ -59,6 +66,34 @@ pub struct BroadcastNode {
     /// A list of messages that have been seen and processed by this node. This
     /// vector is used to prevent re-broadcasting
     seen: HashSet<u32>,
+
+    /// The grace period for which a gossip won't be triggered.
+    ///
+    /// When handling a message, if the current time is greater than this timer,
+    /// a gossip event will be triggered within the handle function and a new
+    /// grace period will be calculated.
+    gossip_timer: Instant,
+}
+
+impl BroadcastNode {
+    /// Send a `Read` request to all neighbors of this node
+    fn gossip(&mut self, out: &mut impl Write) -> anyhow::Result<()> {
+        // If the grace period isn't over yet, don't gossip
+        if self.gossip_timer > Instant::now() {
+            return Ok(());
+        }
+
+        // Grace period is over; send out the reads to our neighbors
+        for neighbor in self.topology.iter() {
+            Message::new(self.id.clone(), neighbor.clone(), None, Payload::Read)
+                .send(out)?;
+        }
+
+        // Calculate a new grace period
+        self.gossip_timer = Instant::now().checked_add(GOSSIP_TIME)
+            .expect("Gossip grace period overflow");
+        Ok(())
+    }
 }
 
 impl Node<Payload> for BroadcastNode {
@@ -70,25 +105,33 @@ impl Node<Payload> for BroadcastNode {
             id:       init.id,
             topology: HashSet::with_capacity(init.nodes.len()),
             seen:     HashSet::new(),
+            gossip_timer: Instant::now().checked_add(GOSSIP_TIME)
+                .expect("Gossip grace period overflow"),
         })
     }
 
     fn handle(&mut self, mut msg: Message<Payload>, out: &mut impl Write)
         -> anyhow::Result<()>
     {
+        // Trigger a gossip if the timer has elapsed
+        self.gossip(out)?;
+
         // Take out the payload from this message
         let payload = match msg.body.payload.take() {
             Some(payload) => payload,
             _ => return Ok(()),
         };
 
-        // Handle the request and also ignore `Ok`s. In reality, we would make
-        // sure that all nodes are synchronized, but since that's not the part
-        // of the problem, we are safe to ignore these
+        // Handle the request
         match payload {
-            // Ignore OK
-            Payload::ReadOk(_) | Payload::TopologyOk
-                | Payload::BroadcastOk => return Ok(()),
+            // Ignore `OK`s
+            Payload::TopologyOk | Payload::BroadcastOk => return Ok(()),
+
+            // The gossip event generates read requests to our neighbors. If we
+            // get a `ReadOk`, synchronize the seen values
+            Payload::ReadOk(data) => {
+                self.seen.extend(data.messages.iter());
+            },
 
             // If we got a topology, change our topology
             Payload::Topology(mut data) => {
